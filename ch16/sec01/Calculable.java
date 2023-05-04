ba07dd0c57602b3b239bb17318e65fde7a58669e
KangCheolgu <opopqwqw123@gmail.com>
2023-05-04 17:51:38 +0900

    콜렉션제네릭 등등

---
+++ ch16/sec01/Calculable.java
@@ -0,0 +1,7 @@
+package ch16.sec01;
+
+@FunctionalInterface
+public interface Calculable {
+
+	void calcuate(int x,int y);
+}
---
+++ ch16/sec02/exam1/Workable.java
@@ -0,0 +1,5 @@
+package ch16.sec02.exam1;
+
+public interface Workable {
+	void work();
+}
